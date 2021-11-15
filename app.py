588a6991c90de828984484f20c88be451005e3d1
Jamie Hirst <jamie.hirst10@gmail.com>
2021-11-15 17:24:09 +0000

    add book to db function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from models import (Base, session, Book, engine)
 import datetime
 import csv
+import time
 
 
 def menu():
@@ -27,15 +28,38 @@ def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 
     'June', 'July', 'August', 'September', 'October', 'November', 'December']
     split_date = date_str.split(' ') #takes the string formatted dates and splits on the space and turns each information into list element
-    month = int(months.index(split_date[0])) + 1 #takes the index 0 of date list (month in this case) and finds index of equivilent month in months list and returns its index + 1 as that is equal to the month number. ie Jan is index 0 so +1 =1st month
-    day = int(split_date[1].split(',')[0]) #removes comma from the day element of list left from initial string split
-    year = int(split_date[2])
-    return datetime.date(year, month, day)
+    try:
+        month = int(months.index(split_date[0])) + 1 #takes the index 0 of date list (month in this case) and finds index of equivilent month in months list and returns its index + 1 as that is equal to the month number. ie Jan is index 0 so +1 =1st month
+        day = int(split_date[1].split(',')[0]) #removes comma from the day element of list left from initial string split
+        year = int(split_date[2])
+        return_date = datetime.date(year, month, day)
+    except ValueError:
+        input('''
+        \n***** DATE ERROR *****
+        \rThe date format should include a valid Month Day, Year
+        \rEx: January 13, 2003
+        \rpress enter to try again
+        \r************************
+        ''')
+        return
+    else:
+        return return_date
 
 
 def clean_price(price_str):
-    price_float = float(price_str)
-    return int(price_float*100)
+    try:
+        price_float = float(price_str)
+    except ValueError:
+        input('''
+        \n***** PRICE ERROR *****
+        \rThe price should be a number without a currency symbol
+        \rEx: 9.99
+        \rpress enter to try again
+        \r************************
+        ''')
+        return
+    else:
+        return int(price_float*100)
 
 
 def add_csv():
@@ -58,8 +82,25 @@ def app():
     while app_running:
         choice = menu()
         if choice == '1':
-            #add book
-            pass
+            title = input('Title: ')
+            author = input('Author: ')
+            date_error = True
+            while date_error: #handles errors in date formatting
+                date = input('Published Date (Ex: October 25, 2017): ')
+                date = clean_date(date)
+                if type(date) == datetime.date: #conditional on variable data type
+                    date_error = False
+            price_error = True
+            while price_error:
+                price = input('Price (Ex: 35.96): ')
+                price = clean_price(price)
+                if type(price) == int:
+                    price_error = False
+            new_book = Book(title=title, author=author, published_date=date, price=price)
+            session.add(new_book)
+            session.commit()
+            print('Book added')
+            time.sleep(1.5)
         elif choice == '2':
             #view books
             pass
@@ -83,8 +124,8 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    #app()
-    add_csv()
+    add_csv
+    app()
 
     for book in session.query(Book):
         print(book)
